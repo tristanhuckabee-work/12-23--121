cb7fc9925d86d3e6d8698d3daebcfabd215ba8b7
tristan <thuckabee.work@gmail.com>
2024-02-26 18:12:30 -0600

    done

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,21 +1,39 @@
 function ageSort(users) {
-  // Your code here 
+  return users.sort((a, b) => a.age - b.age);
 }
 
 function oddEvenSort(arr) {
-  // Your code here 
+  return arr.sort((a, b) => {
+    if ( (a % 2) && !(b % 2)) return -1;
+    if (!(a % 2) &&  (b % 2)) return 1;
+    return a - b;
+  })
 }
 
 function validAnagrams(s, t) {
-  // Your code here 
+  return s.split('').sort().join('') == t.split('').sort().join('');
 }
 
 function reverseBaseSort(arr) {
-  // Your code here 
+  return arr.sort((a, b) => {
+    if (`${a}`.length > `${b}`.length) return -1;
+    if (`${a}`.length < `${b}`.length) return 1;
+    return a - b;
+  })
 }
 
 function frequencySort(arr) {
-  // Your code here 
+  let count = {}
+  arr.forEach(el => {
+    if (count[el]) count[el]++
+    else count[el] = 1;
+  });
+
+  return arr.sort((a, b) => {
+    if (count[a] < count[b]) return -1;
+    if (count[a] > count[b]) return 1;
+    return b - a;
+  })
 }
 
 module.exports = [
